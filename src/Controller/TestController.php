8b9e2f708ba0c2fdbc8325de1fcfab85f431e017
Olivier Joubert <olivier.joubert@gmail.com>
2021-06-28 22:49:50 +0200

    Upload test planet + fixing images

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    public const COMPAT = [10, 30, 10, 40, 10, 90, 90, 80, 100, 0];
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'compat' => self::COMPAT,
+        ]);
+    }
+}
