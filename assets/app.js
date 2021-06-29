d65cfbb2355c0c3cf7b360c7d14a2b61e69f635b
A-david1 <antoine.david.pro1@gmail.com>
2021-06-29 21:19:15 +0200

    add question + answer in twig variables

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,12 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import './script/quizz';
 
 // start the Stimulus application
 import './bootstrap';
 
 const $ = require('jquery');
 require('bootstrap');
+
+
